c0deb9625baecdedf09414dca11f0906635348f2
Dmytro Marych <dimamarich07@gmail.com>
2025-08-05 15:29:59 +0300

    private notes

--- myproject/notes/urls.py
+++ myproject/notes/urls.py
@@ -5,6 +5,6 @@ app_name='notes'
 
 urlpatterns = [
     path('',views.notes_list, name='list'),
-    path('new_note',views.new_note, name='new'),
-    path('/<int:id>',views.note_page, name='page'),
+    path('new_note',views.note_new, name='new'),
+    path('<int:id>',views.note_page, name='page'),
 ]
--- myproject/notes/views.py
+++ myproject/notes/views.py
@@ -1,18 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Note
+from django.contrib.auth.decorators import login_required
+from . import forms
+
 
 # Create your views here.
 
+@login_required(login_url='/users/sign_in/')
 def notes_list(request):
-    notes = Note.objects.all().order_by('-created_at')
-    return render(request, 'notes/notes_list.html', {'notes': notes})
+    user_notes = Note.objects.filter(user_id_id=request.user.id).order_by('-created_at')
+    return render(request, 'notes/notes_list.html', {'notes': user_notes})
 
 def note_page(request,id):
     note = Note.objects.get(id=id)
     return render(request, 'notes/note_page.html', {'note': note})
 
-def new_note(request):
+@login_required(login_url="/users/sign_in/")
+def note_new(request):
     if request.method == 'POST':
+        form = forms.CreateNote(request.POST, request.FILES)
+        if form.is_valid():
+            newnote = form.save(commit=False)
+            newnote.author = request.user
+            newnote.save()
+            return redirect('notes:list')
+    else:
+        form = forms.CreatePost()
+    return render(request, 'notes/new.html',{'form': form})
 
 
 
